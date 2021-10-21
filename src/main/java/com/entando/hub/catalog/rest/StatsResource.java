82873168b57b57d1e6d5f8b9e44568c3313290ed
Anthony Viard <anth.viard@gmail.com>
2021-10-21 17:10:29 +0200

    Add more commits in the data

--- src/main/java/com/entando/hub/catalog/rest/StatsResource.java
+++ src/main/java/com/entando/hub/catalog/rest/StatsResource.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -12,9 +13,19 @@ public class StatsResource {
     @CrossOrigin
     @GetMapping("/commits")
     public Stats getCommits() {
-        Map data = new HashMap();
+        Map<String, Integer> data = new LinkedHashMap<>();
         data.put("January", 89);
         data.put("February", 53);
+        data.put("March", 17);
+        data.put("April", 105);
+        data.put("May", 28);
+        data.put("June", 84);
+        data.put("July", 152);
+        data.put("August", 3);
+        data.put("September", 67);
+        data.put("October", 120);
+        data.put("November", 10);
+        data.put("December", 29);
 
         return new Stats("Commits", data);
     }
