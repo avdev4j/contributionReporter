79ad6cd5db460be221b2cc99da4886dc6191353d
Anthony Viard <anth.viard@gmail.com>
2021-10-21 17:30:06 +0200

    Add bar chart to display the commits

--- ui/widgets/widgets-dir/et-widget/src/App.js
+++ ui/widgets/widgets-dir/et-widget/src/App.js
@@ -1,30 +1,23 @@
 import {getData} from "./integration/Integration";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {Chart} from "./components/Chart";
+
+let loaded = false;
 
 function App() {
-    const [payload, setPayload] = useState("")
-    async function callTheApi() {
-        const response = (await getData());
-        setPayload(response.data)
-    }
-    function _renderData(data) {
-       if(data) {
-            return Object.entries(data).map(([key,value])=>{
-              return (
-                  <div>{key} : {value.toString()}</div>
-              );
-            })
+    const [payload, setPayload] = useState({});
+
+    useEffect(async () => {
+        if(!loaded) {
+            setPayload((await getData()).data);
+            loaded = true;
         }
-    }
+    });
 
     return (
         <>
             <div>
-                <button onClick={callTheApi}>call the api</button>
-            </div>
-            <div>
-                <span>{payload.name}</span>
-                <span>{_renderData(payload.data)}</span>
+                <Chart payload={payload} />
             </div>
         </>
     )
